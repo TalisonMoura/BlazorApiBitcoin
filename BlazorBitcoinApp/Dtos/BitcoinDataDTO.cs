189c60b40c64a36ab7c1884c009ec072b5a2d768
Talison Moura <talisonmoura13@gmail.com>
2023-09-07 21:49:32 -0300

    Criando aplicação em Blazor para consumir API de Bitcoin

---
+++ BlazorBitcoinApp/Dtos/BitcoinDataDTO.cs
@@ -0,0 +1,16 @@
+﻿namespace BlazorBitcoinApp.Dtos
+{
+    public class BitcoinDataDTO
+    {
+        public DateTime Day { get; set; }
+        public decimal CloseValue { get; set; }
+
+        public BitcoinDataDTO() { }
+
+        public BitcoinDataDTO(DateTime day, decimal closeValue)
+        {
+            Day = day;
+            CloseValue = closeValue;
+        }
+    }
+}
---
+++ BlazorBitcoinApp/Services/IBitcoinService.cs
@@ -0,0 +1,9 @@
+﻿using BlazorBitcoinApp.Dtos;
+
+namespace BlazorBitcoinApp.Services
+{
+    public interface IBitcoinService
+    {
+        Task<List<BitcoinDataDTO>> FindByDate(DateTime startDate);
+    }
+}
